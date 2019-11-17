0e82f8a6f4da605fde207f4f372924d144c424ac
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-11-17 23:08:53 +0300

    tests

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+import { extractForecast, extractErrorMessage, getWeather, displayWeather, displayErrorMessage } from "../work";
 
-
-const WEATHER = document.getElementById("weather-container");
-const ERROR = document.getElementById("error-container");
 
 window.onload = () => {
     const input = document.getElementById("input-form");
     input.addEventListener("submit", onSubmit);
+    console.log("here");
 };
 
 function onSubmit(e) {
+  console.log("here2");
     e.preventDefault();
     const city = e.currentTarget.elements.inputfield.value;
     getWeather(city)
         .then(response => {
             response.json()
             .then(json => {
+              console.log("here3");
                 if (response.ok) {
                     const forecast = extractForecast(json);
                     displayWeather(forecast);
@@ -27,98 +28,4 @@ function onSubmit(e) {
             });
         },
         error => displayErrorMessage(error));
-}
-
-function getWeather(city) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="
-        + city +
-        "&appid=e972dcd233bab1ebce419c370711921f&units=metric&lang=en";
-    return fetch(url);
-}
-
-function extractForecast(json) {
-    const {
-        name: city,
-        main:
-        {
-            temp: temperature,
-            pressure,
-            humidity,
-        } = {},
-        wind:
-        {
-            speed: wind,
-        } = {},
-        clouds:
-        {
-            all: clouds,
-        } = {},
-        visibility: visibility,
-    } = json;
-
-    let forecast =
-    {
-        city: "Weather in " + city,
-        description: " is " + json.weather[0].description,
-        parameters:
-            [
-                {
-                    name: "Temperature",
-                    value: temperature,
-                    units: "&deg;C"
-                },
-                {
-                    name: "Pressure",
-                    value: pressure,
-                    units: "hPa",
-                },
-                {
-                    name: "Wind speed",
-                    value: wind,
-                    units: "m/s",
-                },
-                {
-                    name: "Humidity",
-                    value: humidity,
-                    units: "%",
-                },
-                {
-                    name: "Clouds",
-                    value: clouds,
-                    units: "%",
-                },
-                {
-                    name: "Visibility",
-                    value: visibility,
-                    units: "m",
-                },
-            ],
-    };
-
-    return forecast;
-}
-
-function extractErrorMessage(error) {
-  let message = {
-    message: error,
-  }
-
-  return message;
-}
-
-function displayWeather(forecast) {
-    let source = document.getElementById("weather-template").innerHTML;
-    let template = Handlebars.compile(source);
-
-    let html = template(forecast);
-    document.getElementById("weather-container").innerHTML = html;
-}
-
-
-function displayErrorMessage(message) {
-  let source = document.getElementById("message-template").innerHTML;
-  let template = Handlebars.compile(source);
-
-  let html = template(message);
-  document.getElementById("message-container").innerHTML = html;
 }
---
+++ test/test.js
@@ -0,0 +1,97 @@
+const { expect } = require('chai');
+const { extractForecast, extractErrorMessage, getWeather } = require('../work');
+
+
+
+describe('Getting data from response', () => {
+
+  const response = {
+    "coord":{"lon":37.62,"lat":55.75},
+    "weather":[{"id":701,"main":"Mist","description":"mist","icon":"50n"}],
+    "base":"stations",
+    "main":{"temp":4.38,"pressure":1015,"humidity":100,"temp_min":2,"temp_max":5.56},
+    "visibility":3000,
+    "wind":{"speed":4,"deg":90},
+    "clouds":{"all":90},
+    "dt":1573170827,
+    "sys":{"type":1,"id":9027,"country":"RU","sunrise":1573188558,"sunset":1573220232},
+    "timezone":10800,
+    "id":524901,
+    "name":"Moscow",
+    "cod":200
+  };
+
+  it('get from response with existing city', () => {
+    const exp =
+    {
+        cod: 200,
+        city: "Weather in Moscow",
+        description: " is mist",
+        parameters:
+            [
+                {
+                    name: "Temperature",
+                    value: 4.38,
+                    units: "&deg;C"
+                },
+                {
+                    name: "Pressure",
+                    value: 1015,
+                    units: "hPa",
+                },
+                {
+                    name: "Wind speed",
+                    value: 4,
+                    units: "m/s",
+                },
+                {
+                    name: "Humidity",
+                    value: 100,
+                    units: "%",
+                },
+                {
+                    name: "Clouds",
+                    value: 90,
+                    units: "%",
+                },
+                {
+                    name: "Visibility",
+                    value: 3000,
+                    units: "m",
+                },
+
+            ],
+    };
+
+    const real = extractForecast(response);
+    expect(real).to.eql(exp);
+  });
+
+  it('get from response with not existing city', () => {
+    const response = "city not found";
+    const exp = {
+      "message":"city not found"
+    };
+    const real = extractErrorMessage(response);
+    expect(real).to.eql(exp);
+  });
+
+  it("6 elements from response with existing city", function() {
+    const elements = 6;
+    const forecast = extractForecast(response);
+    let len = 0;
+    forecast.parameters.forEach(parameter => {
+      len = len + 1;
+    });
+    expect(len).to.eql(elements);
+  });
+
+  it("elements from response contains 'name', 'value', 'units'", function() {
+    const forecast = extractForecast(response);
+    forecast.parameters.forEach(parameter => {
+      expect(parameter).to.be.an('object').that.have.property("name");
+      expect(parameter).to.be.an('object').that.have.property("value");
+      expect(parameter).to.be.an('object').that.have.property("units");
+    });
+  });
+});
